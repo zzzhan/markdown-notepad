9afda04b0d72908b8a188671d9373f916744c3ad
zzzhan <imailer@sina.com>
2016-04-26 01:02:00 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> v<%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        expand: true,
+        cwd: 'src/js/',
+        src: '**/*',
+        dest:'dist/js/'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'src/js/*.js'
+      ]
+    },
+	cssmin: {
+      options: {
+        report: 'gzip'
+      },
+      build: {
+		files: {		
+			'dist/css/<%= pkg.name %>.min.css':'src/css/<%=pkg.name %>.css'
+		}
+      }
+	},
+    concat: {
+      options: {
+        separator: '',
+        stripBanners: true
+      },
+      css:{
+        src: [
+          'dist/css/*.css'
+        ],
+        dest: 'dist/css/<%=pkg.name %>.all.css'
+      },
+      js:{
+        src: [
+          'dist/js/*.js'
+        ],
+        dest: 'dist/js/<%=pkg.name %>.all.js'
+      }
+	},
+    clean: ['dist']
+  });
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+  grunt.registerTask('default', ['jshint','clean','uglify','cssmin','concat']);
+};
